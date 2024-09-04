1f51c69ad732824d5a257e5be30dce4e51787a8c
caiofmbarbosa <caiofelipebarbosa92@gmail.com>
2024-09-04 20:32:03 -0300

    #

--- scripts/login.php
+++ scripts/login.php
@@ -7,6 +7,7 @@ if ($user === '55') {
     $_SESSION['loggedin'] = true;
     header("Location: ../usuario.php");
     exit();
+    
 } else {
     echo "<script>alert('Usuário não encontrado.'); window.location.href='../index.html';</script>";
     exit();
